9a79d270cb053970e2c21f6949f8da1d52bea1db
Kobus Viljoen <kobusv@gmail.com>
2022-12-04 07:43:36 +0100

    Added solution for day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  alias AdventOfCode.Utils
+
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(&complete_overlap/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse()
+    |> Enum.map(&partial_overlap/1)
+    |> Enum.sum()
+  end
+
+  def complete_overlap([[start_a, end_a], [start_b, end_b]]) do
+    cond do
+      start_a >= start_b and end_a <= end_b -> 1
+      start_b >= start_a and end_b <= end_a -> 1
+      true -> 0
+    end
+  end
+
+  def partial_overlap([[start_a, end_a], [start_b, end_b]]) do
+    cond do
+      start_a >= start_b and start_a <= end_b -> 1
+      end_a >= start_b and end_a <= end_b -> 1
+      start_b >= start_a and start_b <= end_a -> 1
+      end_b >= start_a and end_b <= end_a -> 1
+      true -> 0
+    end
+  end
+
+  def parse(input) do
+    input
+    |> Utils.parse()
+    |> Enum.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> Utils.parse(",")
+    |> Enum.map(&Utils.parse_int(&1, "-"))
   end
 end
--- lib/advent_of_code/utils.ex
+++ lib/advent_of_code/utils.ex
@@ -4,9 +4,20 @@ defmodule AdventOfCode.Utils do
     |> String.split("\n", trim: true)
   end
 
+  def parse(input, sep, options \\ [trim: true]) do
+    input
+    |> String.split(sep, options)
+  end
+
   def parse_int(input) do
     input
     |> parse
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def parse_int(input, sep, options \\ [trim: true]) do
+    input
+    |> parse(sep, options)
     |> Enum.map(&String.to_integer/1)
   end
 end

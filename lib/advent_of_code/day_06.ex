1b98cd2ed0eef235176c0a1a1f99baf0b40755fb
Kobus Viljoen <kobusv@gmail.com>
2022-12-06 07:23:00 +0100

    Added day 6 solutions

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,18 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input), do: solve(input, 4)
+
+  def part2(input), do: solve(input, 14)
+
+  defp solve(data_stream, max_num) do
+    data_stream
+    |> split()
+    |> Enum.with_index()
+    |> Enum.find(fn {_, i} ->
+      parts = split(String.slice(data_stream, i, max_num))
+      length(Enum.uniq(parts)) == max_num
+    end)
+    |> then(fn {_, i} -> i + max_num end)
   end
 
-  def part2(_args) do
-  end
+  defp split(input), do: String.split(input, "", trim: true)
 end

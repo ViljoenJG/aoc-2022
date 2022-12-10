46308730b0de1a1f016772f125b930c2acd49a9f
Kobus Viljoen <kobusv@gmail.com>
2022-12-10 11:10:28 +0100

    Added day 9 solutiosn

--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,19 +3,33 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    R 4
+    U 4
+    L 3
+    D 1
+    R 4
+    D 1
+    L 5
+    R 2
+    """
 
-    assert result
+    assert part1(input) == 13
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    input = """
+    R 5
+    U 8
+    L 8
+    D 3
+    R 17
+    D 10
+    L 25
+    U 20
+    """
 
-    assert result
+    assert part2(input) == 36
   end
 end
